b050511a9a263a2997e779615ce43ae2a23413e3
marthuz <johnson.chesoni@outlook.com>
2022-04-28 10:02:34 +0300

    initial commit

---
+++ footer.php
@@ -0,0 +1,61 @@
+  <footer class="footer-2">
+    <div class="container">
+      <div class="row"> 
+        
+        <!-- Support -->
+        <div class="col-sm-7">
+          <h6>About Us</h6>
+          <div class="about-foot">
+            <p>A concern of Care and Cure Group is the only multi-disciplinary super-specialty tertiary care hospital in Kilifi, confidently providing comprehensive therapy sessions. These services are provided by expert medical professionals, skilled nurses and technologists using state-of-the-art technology. </p>
+          </div>
+        </div>
+        
+       
+        
+        <!-- Conatct -->
+        <div class="col-sm-5">
+          <div class="con-info">
+            <h6>Contact Us</h6>
+            <p>1942  BAMBA, KILIFI</p>
+            <ul>
+              <li class="mobile">
+                <p>0722565656</p>
+              </li>
+              <li class="email">
+                <p>purietheraputic@gmail.com</p>
+                <p>info@purietheraputic.com</p>              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  
+  <!-- Rights -->
+  <div class="rights style-2">
+    <div class="container">
+      <p>© <?php echo date("Y"); ?> Purie Theraputic </p>
+    </div>
+  </div>
+  
+  <!-- GO TO TOP  --> 
+  <a href="#" class="cd-top"><i class="fa fa-angle-up"></i></a> 
+  <!-- GO TO TOP  --> 
+</div>
+<!-- End Page Wrapper --> 
+
+<!-- JavaScripts --> 
+<script src="js/vendors/jquery/jquery.min.js"></script> 
+<script src="js/vendors/wow.min.js"></script> 
+<script src="js/vendors/bootstrap.min.js"></script> 
+<script src="js/vendors/own-menu.js"></script> 
+<script src="js/vendors/jquery.sticky.js"></script> 
+<script src="js/vendors/owl.carousel.min.js"></script> 
+<script src="js/vendors/jquery.magnific-popup.min.js"></script> 
+
+<!-- SLIDER REVOLUTION 4.x SCRIPTS  --> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.t.min.js"></script> 
+<script type="text/javascript" src="rs-plugin/js/jquery.tp.min.js"></script> 
+<script src="js/main.js"></script>
+</body>
+</html>
